d7217ac922f7912b92686977eb3dfd09752c3ecd
olebassen <basseno@gmail.com>
2024-12-24 10:58:20 +0100

    Aktualisieren von menu.js

--- js/menu.js
+++ js/menu.js
@@ -80,3 +80,18 @@ function loadPage(url) {
             contentArea.innerHTML = `<p>Fehler beim Laden der Seite: ${error.message}</p>`;
         });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const infoboxes = document.querySelectorAll('.infobox');
+
+    infoboxes.forEach((button) => {
+        button.addEventListener('click', function () {
+            const content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    });
+});
